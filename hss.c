3c7cde875026e936baf8135fa5998504df0f161d
Jorge Lucas <Jorge Lucas>
2017-07-05 01:54:04 -0300

    Acabado

--- hss.c
+++ hss.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-#define LIM 100000
+#define LIM 1001313
 
 long int POWERS = 0;
 
@@ -62,14 +63,54 @@ void lerValores(char *url, int *C, int tam){
 	fclose(arq);
 }
 
-void imprimir(int *C, int tam){
+char *replace_str(char *str, char *orig, char *rep)
+{
+  static char buffer[4096];
+  char *p;
+
+  if(!(p = strstr(str, orig)))  // Is 'orig' even in 'str'?
+    return str;
+
+  strncpy(buffer, str, p-str); // Copy characters from 'str' start to 'orig' st$
+  buffer[p-str] = '\0';
+
+  sprintf(buffer+(p-str), "%s%s", rep, p+strlen(orig));
+
+  return buffer;
+}
+
+void imprimir(int *C, int tam, float temp){
     int i;
-    printf("\n-------------------\n");
-    printf("Valores:\n");
-    printf("-------------------\n");
+    FILE *arq;
+    char *url;
+    char str[10];
+    sprintf(str,"%d",tam);
+    url = (char*)malloc(sizeof(char)*100);
+    strcpy(url, "hssseq-");
+    strcat(url, str);
+    strcat(url, "elementos-");
+    strcat(url, __DATE__);
+    strcat(url, "-");
+    strcat(url, replace_str(replace_str(__TIME__,":","-"),":","-"));
+    strcat(url, ".txt");
+    printf("Arquivo de Saida:%s\n",url);
+    arq = fopen(url,"r");
+    if(arq == NULL)
+        arq = fopen(url,"wt");
+    else
+        arq = fopen(url,"a");
+    if(arq == NULL){
+        printf("Erro, nao foi possivel criar o arquivo\n");
+        exit(1);
+    }
+    fprintf(arq,"Tempo:%f\n",temp);
+    fprintf(arq,"\n-------------------\n");
+    fprintf(arq,"Valores:\n");
+    fprintf(arq,"-------------------\n");
     for(i=0;i < tam;i++)
-        printf("%d\n",C[i]);
-    printf("-------------------\n\n");
+        fprintf(arq,"%d\n",C[i]);
+    fprintf(arq,"-------------------\n\n");
+    fclose(arq);
 }
 
 int main(int argc, char *argv[]){
@@ -88,10 +129,9 @@ int main(int argc, char *argv[]){
 	lerValores(url,C,tam);
 	clock_t tempo;
 	tempo = clock();
-    heapSort(C,tam);
+    heapSort(C,tam-1);
     float temp = (clock() - tempo) / (double)CLOCKS_PER_SEC;
-    imprimir(C,tam);
-    printf("Tempo:%f\n",temp);
+    imprimir(C,tam,temp);
     free(C);
 	exit(1);
 }
